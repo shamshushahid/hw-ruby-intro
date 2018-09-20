887f3187f6840db8195917baf0ebd4d7fd365759
EC2 Default User <ec2-user@ip-172-31-17-66.us-east-2.compute.internal>
2018-09-20 20:27:04 +0000

    HW_1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,67 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  if (arr.length == 0)
+    sum=0
+  else
+    sum=arr.reduce(:+)
+  end
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum_2=0
+  if (arr.length == 0)
+    return sum_2
+  elsif (arr.length==1)
+    return arr[0]
+  end
+  arr.sort!.reverse!
+  sum_2 = arr[0] + arr[1]
+  return sum_2
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  
+  return false if arr.length == 0
+  arr.each_with_index{|val,index|
+    return true if arr.find_index(n-val) && arr.find_index(n-val)!=index }  
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+ "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  /^[b-df-hj-np-tv-z]/i.match(s) != nil
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s =~/^[0-1]+$/
+    return s.to_i(2)%4 == 0
+  end 
+  false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+  attr_accessor :price
+  
+  def initialize(isbn, price)
+		raise ArgumentError, 
+			"Illegal Argument!!" if isbn.empty?  or price <= 0
+		@isbn = isbn
+		@price = price
+	end
+	
+	def price_as_string
+    format("$%.2f", @price)
+  end
+  
 end
